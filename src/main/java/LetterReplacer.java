50165d05697d8449acd2e14ede49d1df61878756
Jeffrey Ruder <ruderjt@gmail.com>
2016-01-07 20:04:11 -0800

    Pass short word test

--- src/main/java/LetterReplacer.java
+++ src/main/java/LetterReplacer.java
@@ -1,7 +1,7 @@
 public class LetterReplacer {
 
     public String letterReplace(String inputString) {
-        String outputString = new String();
+        String outputString = inputString.replaceAll("[aeiou]", "-");
         return outputString;
     }
 
